c1c2668ebe96b9d513222d2ea806ec2445225f78
jicibils <jicibils@hotmail.com>
2015-04-24 12:44:32 -0300

    update pom.xml (java 1.7)

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -15,12 +15,12 @@ public class App
 
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_development", "root", "root");
 
-        User u = new User();
-        u.set("first_name", "Ariel");
-        u.save();
+         User u = new User();
+         u.set("first_name", "Ariel");
+         u.save();
 
 
-        Base.close();
+         Base.close();
         	
     }
 }
