e458150802e56e39d51154b11d0796fa216cb40d
nikd93 <nik_d93@live.com.ar>
2015-05-08 10:53:02 -0300

    solucion conflictos

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -14,11 +14,11 @@ public class App
         System.out.println( "Hello World!" );
 
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_test", "root", "root");
-        User u = new User();
-        u.set("first_name", "luciano");
-        u.set("last_name","juarez");
-        u.set("email","lucho.juarez79@gmail.com");
-        u.save();
+        // User u = new User();
+        // u.set("first_name", "luciano");
+        // u.set("last_name","juarez");
+        // u.set("email","lucho.juarez79@gmail.com");
+        // u.save();
         
         Base.close();
         	
