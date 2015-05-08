80971f6b5dc41a397f036624b95d9c7bb419fb9b
lucho <lucho.juarez79@gmail.com>
2015-05-08 12:45:48 -0300

    update

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -1,6 +1,7 @@
 package com.unrc.app;
 
 import com.unrc.app.User;
+import com.unrc.app.Rank;
 import org.javalite.activejdbc.Base;
 
 /**
@@ -13,12 +14,12 @@ public class App
     {
         System.out.println( "Hello World!" );
 
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_test", "root", "root");
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_development", "root", "root");
         User u = new User();
-        u.set("first_name", "luciano");
-        u.set("last_name","juarez");
-        u.set("email","lucho.juarez79@gmail.com");
-        u.save();
+        //u.set("first_name", "luciano");
+      //  u.set("last_name","juarez");
+       // u.set("email","lucho.juarez79@gmail.com");
+        //u.save();
         
         Base.close();
         	
---
+++ src/main/java/com/unrc/app/Grid.java
@@ -0,0 +1,10 @@
+package com.unrc.app;
+
+import org.javalite.activejdbc.Model;
+
+public class Grid extends Model {
+   static {
+   }
+   
+
+}
