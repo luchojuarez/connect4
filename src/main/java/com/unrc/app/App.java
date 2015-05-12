41e35dd3120e6610065f13a90984bc65bc72a2bf
jicibils <jicibils@hotmail.com>
2015-05-12 11:24:39 -0300

    fixthem App.java and schema dataBases

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -2,6 +2,11 @@ package com.unrc.app;
 
 import com.unrc.app.User;
 import com.unrc.app.Rank;
+import com.unrc.app.Grid;
+import com.unrc.app.Cell;
+import com.unrc.app.Game;
+import com.unrc.app.Hit;
+import com.unrc.app.Removed;
 import org.javalite.activejdbc.Base;
 /**
  **Hello world!
@@ -15,8 +20,8 @@ public class App
 		Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_test", "root", "root");
 		User u = new User();
 		Rank r = new Rank();
-		u.set("first_name", "Nicolas");
-		u.set("last_name","Dominguez");
+		u.set("nameUs", "Nicolas");
+		u.set("lastNameUs","Dominguez");
 		u.set("email","lucho.juarez79@gmail.com");
 		u.save();
 		r.set("points","6");
