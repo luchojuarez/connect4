e8b0309f8e199dc5bfa781876537d30f04d2c5ad
jicibils <jicibils@hotmail.com>
2015-05-29 17:52:44 -0300

    java 8/pom.xml/html/servidor web/connect4 en la nube

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -10,21 +10,34 @@ import com.unrc.app.Game;
 import com.unrc.app.Hit;
 import com.unrc.app.Removed;
 import org.javalite.activejdbc.Base;
+import static spark.Spark.*;
 /**
  **Hello world!
  **
  */
 public class App
 {
-	private User player1;
-	private User player2;
+	// private User player1;
+	// private User player2;
     public static void main( String[] args )
     {
-		Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_development", "root", "root");
-        System.out.println( "Hello World!" );
-        System.out.println();
-		Start.begin();
-		Base.close();   	
-        System.out.println( "GoodBye!! XD" );
+            get("/hello", (req, res) -> {
+                String response = "<h1> Connect 4</h1>";
+                response  += "<a href =\"/play\"> Jugar</a>";
+                return response;
+             });   
+
+            get("/play", (request, response) -> {
+//            response.status(401);
+            return "Go Away!!!";
+        });
+
     }
+
+		// Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_development", "root", "root");
+  //       System.out.println( "Hello World!" );
+  //       System.out.println();
+		// Start.begin();
+		// Base.close();   	
+  //       System.out.println( "GoodBye!! XD" );
 }
