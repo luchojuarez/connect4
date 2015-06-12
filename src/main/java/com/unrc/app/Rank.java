2a97cb9757830178341c876fe6d4ef60227d60e8
jicibils <jicibils@hotmail.com>
2015-06-12 11:58:42 -0300

    muestra ranking y implementado before y after

--- src/main/java/com/unrc/app/Rank.java
+++ src/main/java/com/unrc/app/Rank.java
@@ -3,6 +3,17 @@ package com.unrc.app;
 import org.javalite.activejdbc.Model;
 
 public class Rank extends Model {
+
+   // @Override
    
+   	public String toStringR1 (){
+    	return this.getString("nroRank");
+	}   
+   	public String toStringR (){
+    	return this.getString("points");
+	}   
+   	public String toStringR2 (){
+    	return this.getString("user_id");
+	}   
 
 }
